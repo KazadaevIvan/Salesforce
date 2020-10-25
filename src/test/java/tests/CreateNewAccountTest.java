2375cf462ad7f320a2c179753f2362cb45396479
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-25 19:47:05 +0300

    Added account instance

--- src/test/java/tests/CreateNewAccountTest.java
+++ src/test/java/tests/CreateNewAccountTest.java
@@ -1,11 +1,11 @@
 package tests;
 
+import models.Account;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-
 public class CreateNewAccountTest extends BaseTest {
+    Account account;
 
     @BeforeMethod
     public void login() {
@@ -13,45 +13,28 @@ public class CreateNewAccountTest extends BaseTest {
                 .openPage()
                 .isPageOpened()
                 .login("", "");
+        account = new Account("Account", "website", "Analyst", "12345678",
+                "description", "Banking", "20", "ul. Lenina",
+                "Minsk", "Minsk", "220140", "Belarus",
+                "ul. Pushkina", "Minsk", "Minsk", "220018",
+                "Belarus");
     }
 
     @Test
     public void newAccountShouldBeCreated() {
-        homePage
-                .isPageOpened();
-        tab
-                .openTab("Accounts")
-                .isTabOpened("Accounts")
-                .clickCreateNew();
-
-        String accountName = "Account for testing";
-
-        modal
-                .isModalOpened("Accounts")
-                .inputWithSearchSendKeys("Account Name", accountName)
-                .selectOption("Type", "Investor")
-                .inputSendKeys("Website", "123@mail.ru")
-                .inputSendKeys("Phone", "12345678")
-                .selectOption("Industry", "Apparel")
-                .inputSendKeys("Employees", "4")
-                .textAreaSendKeys("Description", "abcsadkjgsf")
-                .textAreaSendKeys("Billing Street", "Lenina street")
-                .textAreaSendKeys("Shipping Street", "Octiabrskaya street")
-                .inputSendKeys("Billing City", "Minsk")
-                .inputSendKeys("Billing State/Province", "Minsk")
-                .inputSendKeys("Billing Zip/Postal Code", "220-118")
-                .inputSendKeys("Billing Country", "Belarus")
-                .inputSendKeys("Shipping City", "Minsk")
-                .inputSendKeys("Shipping State/Province", "Minsk")
-                .inputSendKeys("Shipping Zip/Postal Code", "220-109")
-                .inputSendKeys("Shipping Country", "Belarus")
-                .submitForm();
-        homePage
-                .isPageOpened();
-        tab
-                .openTab("Accounts")
-                .isTabOpened("Accounts");
-        assertEquals(tab.getFirstName(), accountName, "Account name should be '" +
-                accountName + "'");
+        accountListPage
+                .openPage()
+                .clickNew();
+        newAccountModal
+                .isPageOpened()
+                .create(account)
+                .clickSave();
+        accountDetailsPage
+                .isPageOpened()
+                .validateAccount("Account", "website", "Analyst", "12345678",
+                        "description", "Banking", "20", "ul. Lenina",
+                        "Minsk", "Minsk", "220140", "Belarus",
+                        "ul. Pushkina", "Minsk", "Minsk", "220018",
+                        "Belarus");
     }
 }
