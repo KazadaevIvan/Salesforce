9dc65ba6376b54d7f0f7ee60212a5216e883f1b0
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-25 19:43:43 +0300

    Added pages instances

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import pages.HomePage;
-import pages.LoginPage;
-import pages.Modal;
-import pages.Tab;
+import pages.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
     LoginPage loginPage;
-    HomePage homePage;
-    Tab tab;
-    Modal modal;
+    AccountListPage accountListPage;
+    NewAccountModal newAccountModal;
+    AccountDetailsPage accountDetailsPage;
+    ContactListPage contactListPage;
+    NewContactModal newContactModal;
+    ContactDetailPage contactDetailPage;
 
     @BeforeSuite
     public void beforeSuite() {
@@ -29,16 +29,20 @@ public class BaseTest {
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
+        options.addArguments("--disable-popup-blocking");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
-        homePage = new HomePage(driver);
-        tab = new Tab(driver);
-        modal = new Modal(driver);
+        accountListPage = new AccountListPage(driver);
+        newAccountModal = new NewAccountModal(driver);
+        accountDetailsPage = new AccountDetailsPage(driver);
+        contactListPage = new ContactListPage(driver);
+        newContactModal = new NewContactModal(driver);
+        contactDetailPage = new ContactDetailPage(driver);
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
     }
