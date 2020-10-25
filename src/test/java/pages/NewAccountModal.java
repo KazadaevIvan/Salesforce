d59dd158eb39bef939b1b80354dfc9b68e791394
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-25 19:39:52 +0300

    Added NewAccountModal

---
+++ src/test/java/pages/NewAccountModal.java
@@ -0,0 +1,58 @@
+package pages;
+
+import elements.DropDown;
+import elements.Input;
+import elements.InputWithSearch;
+import elements.TextArea;
+import models.Account;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class NewAccountModal extends BasePage {
+    public final static By ACCOUNT_NAME_POLE = By.xpath("//span[contains(text(),'Sort')]/following-sibling::" +
+            "span[contains(text(),'Account Name')]");
+    public final static By SAVE_BUTTON = By.xpath("//*[@title='Save']");
+
+    public NewAccountModal(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public NewAccountModal isPageOpened() {
+        try {
+            wait.elementToBeVisible(ACCOUNT_NAME_POLE);
+        } catch (TimeoutException e) {
+            Assert.fail("The modal has not been loaded. Pole not found by locator " + ACCOUNT_NAME_POLE);
+        }
+        return this;
+    }
+
+    public NewAccountModal create(Account account) {
+        new InputWithSearch(driver, "Account Name").write(account.getAccountName());
+        new DropDown(driver, "Type").selectOption(account.getType());
+        new Input(driver, "Website").write(account.getWebsite());
+        new Input(driver, "Phone").write(account.getPhone());
+        new TextArea(driver, "Description").write(account.getDescription());
+        new DropDown(driver, "Industry").selectOption(account.getIndustry());
+        new Input(driver, "Employees").write(account.getEmployees());
+        new TextArea(driver, "Billing Street").write(account.getBillingStreet());
+        new Input(driver, "Billing City").write(account.getBillingCity());
+        new Input(driver, "Billing State").write(account.getBillingState());
+        new Input(driver, "Billing Zip").write(account.getBillingZip());
+        new Input(driver, "Billing Country").write(account.getBillingCountry());
+        new TextArea(driver, "Shipping Street").write(account.getShippingStreet());
+        new Input(driver, "Shipping City").write(account.getShippingCity());
+        new Input(driver, "Shipping State").write(account.getShippingState());
+        new Input(driver, "Shipping Zip").write(account.getShippingZip());
+        new Input(driver, "Shipping Country").write(account.getShippingCountry());
+        return this;
+    }
+
+    public AccountListPage clickSave() {
+        wait.elementToBeClickable(SAVE_BUTTON);
+        driver.findElement(SAVE_BUTTON).click();
+        return new AccountListPage(driver);
+    }
+}
