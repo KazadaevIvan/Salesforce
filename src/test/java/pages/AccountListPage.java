065f18a9aff8a810f4e1c54f3662e8d79e4b1cae
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-31 16:30:43 +0300

    Updated isPageOpened method

--- src/test/java/pages/AccountListPage.java
+++ src/test/java/pages/AccountListPage.java
@@ -8,8 +8,8 @@ import org.testng.Assert;
 public class AccountListPage extends BasePage {
     public final static By ACCOUNT_NAME_COLUMN = By.xpath("//span[contains(text(),'Sort')]/following-sibling::" +
             "span[contains(text(),'Account Name')]");
-    String URL = "https://wow4.lightning.force.com/lightning/o/Account/list?filterName=Recent";
-    By newButton = By.cssSelector("[title=New]");
+    public final static String URL = "https://wow4.lightning.force.com/lightning/o/Account/list?filterName=Recent";
+    public final static By NEW_BUTTON = By.xpath("//div[@title='New']");
 
     public AccountListPage(WebDriver driver) {
         super(driver);
@@ -18,9 +18,9 @@ public class AccountListPage extends BasePage {
     @Override
     public AccountListPage isPageOpened() {
         try {
-            wait.elementToBeVisible(ACCOUNT_NAME_COLUMN);
+            wait.elementToBeVisible(NEW_BUTTON);
         } catch (TimeoutException e) {
-            Assert.fail("The page has not been loaded. Table not found by locator " + ACCOUNT_NAME_COLUMN);
+            Assert.fail("The page has not been loaded. Button not found by locator " + NEW_BUTTON);
         }
         return this;
     }
@@ -31,8 +31,8 @@ public class AccountListPage extends BasePage {
     }
 
     public NewAccountModal clickNew() {
-        wait.elementToBeClickable(newButton);
-        driver.findElement(newButton).click();
+        wait.elementToBeClickable(NEW_BUTTON);
+        driver.findElement(NEW_BUTTON).click();
         return new NewAccountModal(driver);
     }
 }
