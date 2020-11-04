8e52e31c1c82c2e8d9a1cf878add8030975cc902
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-11-04 14:12:21 +0300

    Added ContactDetailsPage

---
+++ src/test/java/pages/ContactDetailsPage.java
@@ -0,0 +1,66 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class ContactDetailsPage extends BasePage {
+    public final static By DETAILS = By.xpath("(//*[@title='Details'])[1]/a");
+    public final static By DELETE_BUTTON = By.xpath("//*[@name='Delete']");
+    public final static String detailLocator = "//*[contains(text(),'%s')]/ancestor::div/" +
+            "div[@class='slds-form-element__control']/descendant::*";
+    public final static String baseInfo = "[@data-output-element-id='output-field']";
+    public final static String addressInfo = "[@class='slds-truncate']";
+    public final static String accountName = "/descendant::a/span";
+
+    public ContactDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public ContactDetailsPage isPageOpened() {
+        try {
+            wait.elementToBeVisible(DETAILS);
+        } catch (TimeoutException e) {
+            Assert.fail("The page has not been loaded. Pole not found by locator " + DETAILS);
+        }
+        return this;
+    }
+
+    public ContactDetailsPage openContactDetails() {
+        wait.elementToBeClickable(DETAILS);
+        driver.findElement(DETAILS).click();
+        return this;
+    }
+
+    public String getFullName() {
+        return driver.findElement(By.xpath(String.format("(" + detailLocator + baseInfo + ")[1]", "Name"))).getText();
+    }
+
+    public String getPoleText(String poleName) {
+        return driver.findElement(By.xpath(String.format(detailLocator + baseInfo, poleName))).getText();
+    }
+
+    public String getAddressPoleText(String addressType) {
+        StringBuilder address = new StringBuilder();
+        List<WebElement> list = driver.findElements(By.xpath(String.format(detailLocator + addressInfo, addressType)));
+        for (WebElement line : list) {
+            address.append(line.getText()).append("\n");
+        }
+        return String.valueOf(address);
+    }
+
+    public String getAccountName(String poleName) {
+        return driver.findElement(By.xpath(String.format(detailLocator + baseInfo + accountName, poleName))).getText();
+    }
+
+    public DeleteModal deleteContact() {
+        wait.elementToBeClickable(DELETE_BUTTON);
+        driver.findElement(DELETE_BUTTON).click();
+        return new DeleteModal(driver);
+    }
+}
